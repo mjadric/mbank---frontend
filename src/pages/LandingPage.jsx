e886f5bfce4a4dd0a231a67daf02fd4e89b4c08b
Mia Jadric <mjadric@pmfst.hr>
2024-04-30 19:25:12 +0200

    pic

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -14,7 +14,7 @@ const LandingPage = () => {
         <div className="container text-center">
           <div className="row align-items-center" style={{ paddingTop: '8rem' }}>
             <div className="col-md-6">
-              <img src="src/assets/_.gif" alt="" className="img-fluid w-100" />
+              <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Dblack%2Bcredit%2Bcard%2Btemplate&psig=AOvVaw0rpjCy8b3VXsbswrniZ3g9&ust=1714584291218000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDWoq666oUDFQAAAAAdAAAAABAE" alt="" className="img-fluid w-100" />
             </div>
             <div className="col-md-6">
               <div className="text-center">
